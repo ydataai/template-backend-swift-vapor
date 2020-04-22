d1d9966880e579dcc206f20ecc2a73c33cced9ff
Luís Portela Afonso <luis.portela@ydata.ai>
2020-04-22 09:42:51 +0100

    chore(bootstrap): simple example for bootstrap

---
+++ Sources/App/configure.swift
@@ -0,0 +1,18 @@
+import Fluent
+import FluentMySQLDriver
+import Vapor
+
+// configures your application
+public func configure(_ app: Application) throws {
+    app.databases.use(.mysql(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+    ), as: .mysql)
+
+    app.migrations.add(CreateTodo())
+
+    // register routes
+    try routes(app)
+}
